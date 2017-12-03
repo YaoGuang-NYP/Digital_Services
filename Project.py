717a85e6ae8576cc2330b349ed2052bf408fd010
ashley <astro.7713@gmail.com>
2017-12-03 20:37:44 +0800

    update

--- Project.py
+++ Project.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
+from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, validators
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
